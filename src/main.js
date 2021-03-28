5af629a2e8d5bcb28139bbd9764503c1c37980dd
许世飞 <xusf.data4truth.com>
2021-03-28 16:43:09 +0800

    初始化

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import {createApp} from 'vue'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import locale from 'element-plus/lib/locale/lang/zh-cn' // lang i18n
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from '@/store'
+import router from '@/router'
+
+import '@/icons' // icon
+import '@/permission' // permission control
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online ! ! !
+ */
+if (process.env.NODE_ENV === 'production') {
+  const {mockXHR} = require('../mock')
+  mockXHR()
+}
+const app = createApp(App)
+app.use(ElementPlus, {locale})
+app.use(router)
+app.use(store)
+app.config.productionTip = false
+app.mount('#app')
