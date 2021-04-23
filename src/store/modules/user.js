c93bbd1b2ecf2c628adaf08eacda849b5d189a2e
许世飞 <xusf.data4truth.com>
2021-04-23 14:08:58 +0800

    登录增加验证码

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -30,9 +30,9 @@ const mutations = {
 const actions = {
   // user login
   login({ commit }, userInfo) {
-    const { username, password } = userInfo
+    const { username, password, verifyCode, verifyCodeId } = userInfo
     return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
+      login({ username: username.trim(), password: password, verifyCode: verifyCode, verifyCodeId: verifyCodeId }).then(response => {
         const { data } = response
         commit('SET_TOKEN', data.token)
         setToken(data.token)
