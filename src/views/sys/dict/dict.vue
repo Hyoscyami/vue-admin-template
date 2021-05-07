95873be72e72404e8c59eda9ad0c963cb89557d8
许世飞 <xusf.data4truth.com>
2021-05-07 11:31:40 +0800

    字典管理

--- src/views/sys/dict/dict.vue
+++ src/views/sys/dict/dict.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="app-container">
+    <div class="filter-container">
+      <el-form ref="formInline" :inline="true" :model="formInline" class="demo-form-inline">
+        <el-form-item label="字典值" prop="code">
+          <el-input v-model="formInline.code" placeholder="字典值" />
+        </el-form-item>
+        <el-form-item label="描述" prop="description">
+          <el-input v-model="formInline.description" placeholder="描述" />
+        </el-form-item>
+        <el-form-item label="状态" prop="enable">
+          <el-select v-model="formInline.enable" placeholder="状态">
+            <el-option label="启用" value="true" />
+            <el-option label="禁用" value="false" />
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">查询</el-button>
+          <el-button @click="resetForm('formInline')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
     <el-table
       :data="tableData"
       style="width: 100%"
@@ -18,6 +38,16 @@
         prop="address"
         label="地址"
       />
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="100"
+      >
+        <template #default="scope">
+          <el-button type="text" size="small" @click="handleClick(scope.row)">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -43,7 +73,23 @@ export default {
         date: '2016-05-03',
         name: '王小虎',
         address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      }],
+      formInline: {
+        code: '',
+        description: '',
+        enable: undefined
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!')
+    },
+    handleClick(row) {
+      console.log(row)
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
     }
   }
 }
